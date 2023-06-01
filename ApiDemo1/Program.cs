405e03a329d8c49bee7d8c331e3a3d0c5c2fbaa2
Will <doggy.huang@gmail.com>
2023-06-01 15:19:29 +0800

    改用 builder.Services.AddSingleton<MySample>();

--- ApiDemo1/Program.cs
+++ ApiDemo1/Program.cs
@@ -5,7 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddScoped<MySample>();
+builder.Services.AddSingleton<MySample>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
