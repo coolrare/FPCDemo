3633117ff7e7460b3be07a7b3e489adbb9eaa4c4
Will <doggy.huang@gmail.com>
2023-06-02 12:00:53 +0800

    加入 /api/Departments API

---
+++ EFCoreDemo/Controllers/DepartmentsController.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using EFCoreDemo.Models;
+using Omu.ValueInjecter;
+
+namespace EFCoreDemo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartmentsController : ControllerBase
+    {
+        private readonly ContosoUniversityContext _context;
+
+        public DepartmentsController(ContosoUniversityContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Departments
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Department>>> GetDepartment()
+        {
+            return await _context.Department.ToListAsync();
+        }
+
+        // GET: api/Departments/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Department>> GetDepartment(int id)
+        {
+            var department = await _context.Department.FindAsync(id);
+
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            return department;
+        }
+
+        // PUT: api/Departments/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDepartment(int id, DepartmentUpdateDto department)
+        {
+            var d = await _context.Department.FindAsync(id);
+
+            if (d == null)
+            {
+                return NotFound();
+            }
+
+            d.InjectFrom(department);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DepartmentExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Departments
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Department>> PostDepartment(DepartmentCreateDto department)
+        {
+            var d = new Department();
+            d.InjectFrom(department);
+            _context.Department.Add(d);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDepartment", new { id = d.DepartmentId }, d);
+        }
+
+        // DELETE: api/Departments/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDepartment(int id)
+        {
+            if (_context.Department == null)
+            {
+                return NotFound();
+            }
+            var department = await _context.Department.FindAsync(id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+
+            _context.Department.Remove(department);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DepartmentExists(int id)
+        {
+            return (_context.Department?.Any(e => e.DepartmentId == id)).GetValueOrDefault();
+        }
+    }
+}
