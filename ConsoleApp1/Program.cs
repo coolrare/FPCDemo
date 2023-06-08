97d0935cc0d3b36cd0370d187aea506c6e73b242
Will <doggy.huang@gmail.com>
2023-06-08 11:27:40 +0800

    加入 ConsoleApp1 示範「連線服務」如何直接透過強型別呼叫 API 服務

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,12 @@
+﻿using ConsoleApp1;
+
+var http = new HttpClient();
+
+var client = new swaggerClient("https://localhost:7129", http);
+
+var result = await client.CoursesAllAsync();
+
+foreach (var item in result)
+{
+    Console.WriteLine(item.Title);
+}
